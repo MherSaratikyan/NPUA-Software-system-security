99fa0d6cefb5746d3e7a9bfcd2646584ee4775fc
Saratikyan Mher <saratikyanmher20@gmail.com>
2023-11-27 17:34:52 +0400

    solution of the problem to find the missing number from sequence

---
+++ findthemissing.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
+int getMissingValue(const std::vector<unsigned>& vec){
+    int m = vec.size() + 1;
+    int mSum = m * (m + 1) / 2;
+
+    int nSum{0};
+    for(size_t i{0};i < vec.size();++i){
+        nSum += vec[i];
+    }
+
+    return (mSum - nSum);
+}
+
+int main(){
+    srand(time(0));
+    unsigned n;
+    std::cout<<"Enter n: ";
+    std::cin>>n;
+    
+    std::vector<unsigned> vec;
+    unsigned val = 1 + rand() % n;
+
+    int i{1};
+    while(i <= n){
+        if(i != val){
+            vec.push_back(i);
+        }
+        ++i;
+    }
+
+    std::cout<<"The array:\n";
+    for(size_t i{0};i < vec.size();++i){
+        std::cout<<vec[i]<<' ';
+    }
+
+    std::cout<<"\nThe missing value is "
+            <<getMissingValue(vec);
+}
